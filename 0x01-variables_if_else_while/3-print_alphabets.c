41e7447ba10d55512118832d0d05afed78712c09
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-06 21:15:49 +0000

    Print alphabet with putchar-aA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - main block
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
