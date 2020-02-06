bcafd053ff96f10ca863931ed374591e116eaa53
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-06 20:49:57 +0000

    Print alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphapet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - main block
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+return (0);
+}
