e96eef1c11237b5ecc4ed0ebfc2ce54567b992b2
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-06 22:13:13 +0000

    Print exadecimal number in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - main block.
+ *Description: print reverse alphabet.
+ *Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+putchar(c);
+for (c = 97; c <= 102; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
