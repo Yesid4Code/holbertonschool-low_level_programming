dd33b6cd4f2b0583d943d75319ddb3f86246e93f
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-11 15:57:16 +0000

    Print hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,21 +6,21 @@
 
 void jack_bauer(void)
 {
-  int i, j, la, li, ya, ye;
-  for (i = 0; i < 24; i++)
-    {
-      for (j = 0; j < 60; j++)
-	{
-	  la = i / 10;
-	  li = i % 10;
-	  _putchar(la + '0');
-	  _putchar(li + '0');
-	  _putchar(':');
-	  ye = j / 10;
-	  ya = j % 10;
-	  _putchar(ye + '0');
-	  _putchar(ya + '0');
-	  _putchar('\n');
-	}
-    }
+int i, j, la, li, ya, ye;
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+la = i / 10;
+li = i % 10;
+_putchar(la + '0');
+_putchar(li + '0');
+_putchar(':');
+ye = j / 10;
+ya = j % 10;
+_putchar(ye + '0');
+_putchar(ya + '0');
+_putchar('\n');
+}
+}
 }
