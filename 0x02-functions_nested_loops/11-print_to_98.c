817074ef608ab3656875f757acc6fb2c68ef57e9
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-11 19:45:21 +0000

    print numbers until 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,11 +7,12 @@
  *Return: nothing.
  */
 
-void print_to_98(int n)
+void print_to_98(int i)
 {
-if (n <= 98)
+int n;
+if (i <= 98)
 {
-for (n; n <= 98; n++)
+for (n = i; n <= 98; n++)
 {
 printf("%d", n);
 if (n != 98)
@@ -23,7 +24,7 @@ printf("\n");
 }
 else
 {
-for (n; n >= 98; n--)
+for (n = i; n >= 98; n--)
 {
 printf("%d", n);
 if (n != 98)
