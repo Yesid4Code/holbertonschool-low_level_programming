1d2eb067c89cf14261a9a4691bae149c98ef6af7
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-13 19:21:32 +0000

    print a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,11 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			if (n > 1)
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < i; j++)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
 			_putchar(92);
 			_putchar('\n');
