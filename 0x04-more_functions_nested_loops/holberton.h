395dd0b3a99558ed176c58e29e4b82262e7ede59
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-13 17:16:22 +0000

    Library of this subdirectory

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int c, int d);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int c);
+void print_diagonal(int c);
+void print_square(int size);
+
