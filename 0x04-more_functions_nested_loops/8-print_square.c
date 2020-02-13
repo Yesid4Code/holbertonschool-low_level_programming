6aa9bf517f78a678e9f2d48d3df4b5c166282444
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-13 19:39:09 +0000

    Printing n times a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,16 @@ void print_square(int n)
 {
 	int a, b;
 
-	for (a = 0; a < n; a++)
+	if (n > 0)
 	{
-		for (b = 0; b < n; b++)
-			_putchar(35);
-		_putchar('\n');
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+				_putchar(35);
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 
 }
