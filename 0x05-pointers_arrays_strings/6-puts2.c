0a3cde996e8762ccbe9adab9fc3d59284424f146
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-18 16:08:25 +0000

    printing numbers of an array

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *puts2 - fuction that prints every other character of a string.
+ *@str: string that bring a serie of numbers.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
