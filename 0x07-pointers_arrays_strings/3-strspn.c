bdb97b1e240c7be7f05daa09dab1d5334969c618
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-24 21:48:17 +0000

    count elements

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_strspn - functions that locates a character in a string.
+ *@s: string where should locate the character.
+ *@ac: The character.
+ *Return: a pointer.
+ */
+
+unsigned int _strspn(char *s, char *ac)
+{
+	int i, j;
+	unsigned int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; ac[j] != '\0'; j++)
+		{
+			if (s[i] == ac[j])
+			{
+				c++;
+				break;
+			}
+		}
+		if (c < i)
+			break;
+	}
+	return (c);
+}
