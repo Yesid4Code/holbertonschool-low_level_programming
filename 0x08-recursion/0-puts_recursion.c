9e42ffd18203a78c3de075b4db6a7afd9e38bcf5
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-02-26 20:49:19 +0000

    Print a string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s != '\0')
-		_puts_recursion(s + 1);
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 	else
 		_putchar('\n');
 }
