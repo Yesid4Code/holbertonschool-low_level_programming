760ba3604780dac050eedc37883553953813ca63
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-03-05 21:16:08 +0000

    Concatenate using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - Concatenate two string using malloc.
@@ -15,10 +16,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int m, j = 0;
 	char *p;
 
-/*	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-	s2 = ""; */
 	if (s1)
 	{
 		while (s1[i] != '\0')
