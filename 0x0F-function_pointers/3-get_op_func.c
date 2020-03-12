ac9eee17cfccddd5c0cd76e1406f2e18afdf4b07
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-03-12 00:15:36 +0000

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,7 +10,6 @@
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
-	
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -23,9 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	while (i < 5)
 	{
 		if (*s == *ops[i].op && s[1] == 0)
-		{
-			return(ops[i].f);
-		}
+			return (ops[i].f);
 		i++;
 	}
 	printf("Error\n");
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,12 +4,13 @@
  * main - program that performs simple operations.
  * @argc: the number of arguments.
  * @argv: the array conteined in each position.
+ * Return: always 0.
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1 = 0, num2 = 0;
-	
+
 	if (argc == 4)
 	{
 		num1 = atoi(argv[1]);
@@ -22,4 +23,5 @@ void main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	return (0);
 }
