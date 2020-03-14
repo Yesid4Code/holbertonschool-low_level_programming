ad2a6e729bbad07d9950fe484491a76749a3b294
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-03-14 19:15:22 +0000

    sum parameter with variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		summ += va_arg(summ, int);
+		summ += va_arg(summ, unsigned int);
 	va_end(sum);
 	return (summ);
 }
