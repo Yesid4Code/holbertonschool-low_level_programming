d32a1218a5118844f1c157a7c7cfa5ad228731d1
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-03-14 19:33:52 +0000

    Printing numbers with variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, unsigned int));
-		if (i != (n -1))
+		printf("%d", va_arg(ap, int));
+		if (i < (n -1) && separator)
 			printf("%s", separator);
 	}
 	va_end(ap);
