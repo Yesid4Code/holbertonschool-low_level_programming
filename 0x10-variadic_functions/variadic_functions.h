b256a2174ebb8c70d14453a309cc7921618e0811
Yesid A. Lopez V <Yesid4IT@gmail.com>
2020-03-14 19:28:22 +0000

    Library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,7 +11,7 @@ typedef struct listoftype
 {
 	char *op;
 	void (*f)();
-}lot;
+} lot;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
