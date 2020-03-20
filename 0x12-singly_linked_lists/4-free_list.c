52466397eeb461355326e6b36466a2bd990c13ce
Yesid A. López V <yesid4IT@gmail.com>
2020-03-20 05:01:14 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list -  function that frees a list.
+ * @head: first item in the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+	list_t *auxend;
+
+	aux = head;
+
+	while (aux != NULL)
+	{
+		auxend = aux->next;
+		free(aux->str);
+		free(aux);
+		aux = auxend;
+	}
+}
