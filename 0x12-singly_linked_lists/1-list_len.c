ea021af93745309e8d66c09c3f7503b27a115b4b
Yesid A. López V <yesid4IT@gmail.com>
2020-03-20 03:54:41 +0000

    uploading a file with function that return the number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: pointer to the header of the list.
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
