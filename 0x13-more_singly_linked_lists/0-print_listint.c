bb989b4d86e268a1fb9b56ab2d1824d6a407367d
Yesid A. López V <yesid4IT@gmail.com>
2020-03-23 17:01:54 +0000

    function that prints a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that print numbers of a list.
+ * @h: the address to the header of a list.
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
