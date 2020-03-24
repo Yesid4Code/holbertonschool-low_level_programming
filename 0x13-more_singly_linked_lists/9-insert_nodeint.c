078e82c9ed2ba43051809cb9ffafdf596fd73d6d
Yesid A. López V <yesid4IT@gmail.com>
2020-03-24 15:54:49 +0000

    Deleting a duplicate line

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			nnext = tmp->next;
 			tmp->next = nnode;
-			nnode->n = n;
 			nnode->next = nnext;
 			return (nnode);
 		}
