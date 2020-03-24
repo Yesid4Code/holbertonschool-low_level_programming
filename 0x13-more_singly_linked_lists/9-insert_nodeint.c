7d34cbf6cbb44beaf57614a6cc4d81f362f223dd
Yesid A. López V <yesid4IT@gmail.com>
2020-03-24 15:33:57 +0000

    Defining variables as NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,9 +11,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *nnode;
-	listint_t *nnext;
-	listint_t *tmp;
+	listint_t *nnode = NULL;
+	listint_t *nnext = NULL;
+	listint_t *tmp = NULL;
 
 	nnode = malloc(sizeof(listint_t));
 	if (nnode == NULL)
