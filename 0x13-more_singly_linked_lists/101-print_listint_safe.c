d3a7bf700163ebf950b0518a65a185382606f546
Yesid A. López V <yesid4IT@gmail.com>
2020-03-25 02:16:45 +0000

    Changing the position to the counter

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,8 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 			printf("-> [%p] %d\n", (void *)head, head->n);
 			break;
 		}
-		else
-			head = head->next;
+		head = head->next;
 	}
 	return (i);
 }
