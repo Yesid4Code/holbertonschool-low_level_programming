782cd341690d8b522caa0ee073e6c2602417c690
Yesid A. López V <yesid4IT@gmail.com>
2020-03-27 03:52:11 +0000

    Adding condition on the loop

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mov, i = 0;
 
-	while (n != 0)
+	while (n != 0 && index <= (sizeof(n) * 8))
 	{
 		mov = (n >> 1);
 		mov <<= 1;
