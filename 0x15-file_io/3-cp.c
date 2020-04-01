d901ded7d43d81b03e07cf59d756756235fd5c41
Yesid A. López V <yesid4IT@gmail.com>
2020-04-01 03:53:23 +0000

    Adding the return

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,8 +2,8 @@
 
 /**
  * main - program that copies the content of a file to another file.
- * @filename: is the name of the file.
- * @text_content: string to add at the end of the file.
+ * @argc: number of arguments.
+ * @argv: file to copy.
  *
  * Return: 1: success or -1: failure.
  */
@@ -13,22 +13,13 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_fom file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_fom file_to\n"), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		exit(98);
-	}
-	fd2 = open(argv[2], O_CREAT | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 /* */
 	do {
 		fd1read = read(fd1, buf, 1024);
@@ -57,4 +48,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
+	return (0);
 }
