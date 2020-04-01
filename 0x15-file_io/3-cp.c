3fabc79dcb88c5e84015097ea2026709417896e6
Yesid A. López V <yesid4IT@gmail.com>
2020-04-01 04:11:14 +0000

    Changing the length of the line 19

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_fom file_to\n"), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -38,10 +41,7 @@ int main(int argc, char *argv[])
 	} while (fd1read == 1024);
 
 	if (close(fd1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 
 	if (close(fd2) == -1)
 	{
