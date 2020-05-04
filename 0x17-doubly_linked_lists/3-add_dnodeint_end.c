a3994e7dbbc8f361079d1f5253d4f5bcce1b0422
Yesid A. López V <yesid4IT@gmail.com>
2020-05-04 02:33:21 +0000

    The structure of the first condition changed

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new = NULL, *tmp = NULL;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
