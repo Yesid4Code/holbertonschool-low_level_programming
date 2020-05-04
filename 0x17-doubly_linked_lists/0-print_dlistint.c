0fe16b8eb5de7c77acb8b99276def981e9c8fb08
Yesid A. López V <yesid4IT@gmail.com>
2020-05-04 00:07:00 +0000

    Add function to print value of a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that print the content of a list.
+ * @h: pointer to the head of the list.
+ *
+ * Return: number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
