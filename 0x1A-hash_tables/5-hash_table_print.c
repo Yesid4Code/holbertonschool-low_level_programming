279f8e262c6628409eba09655fd3d7ad45bd28a2
Yesid A. López V <Yesid4IT@gmail.com>
2020-05-29 07:34:30 +0000

    space deleted between two string on line 25

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 				if (flag == 1)
 					printf(", ");
 				flag = 1;
-				printf("'%s' : '%s'", tmp->key, tmp->value);
+				printf("'%s': '%s'", tmp->key, tmp->value);
 				tmp = tmp->next;
 			}
 			index++;
