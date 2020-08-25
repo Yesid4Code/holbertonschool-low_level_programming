765cbc394bfb4126e28fb3060cbbfdd7f1d5f721
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-25 23:44:55 +0000

    Binary search algorithm Replace the recursion whith a while

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,27 +10,28 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, m = 0;
+	size_t i, m = 0, start = 0, end = size - 1;
 
 	if (array)
 	{
-		printf("Searching in array: ");
-		for (i = 0; i < size; i++)
+		while (start <= end)
 		{
-			printf("%d", array[i]);
-			if (i + 1 != size)
-				printf(",");
+			printf("Searching in array: ");
+			for (i = start; i <= end; i++)
+			{
+				printf("%d", array[i]);
+				if (i + 1 < end)
+					printf(", ");
+			}
+			printf("\n");
+			m = ((start + end) / 2);
+			if (value < array[m])
+				end = m - 1;
+			else if (value > array[m])
+				start = m + 1;
+			else
+				return (m);
 		}
-		printf("\n");
-		m = size / 2;
-		if (array[m] == value)
-			return (m);
-		if (value < array[m] && (m - 1) >= 0)
-		{
-			return (binary_search((array), (m - 1), value));
-		}
-		if (m + 1 <= size)
-			return (binary_search((array + m), (size - m - 1), value));
 	}
 	return (-1);
 }
