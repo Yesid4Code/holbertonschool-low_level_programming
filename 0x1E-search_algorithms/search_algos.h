1289687f55f7b1f97f6b0ad8307b71a85314543e
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-25 21:52:24 +0000

    Header Header file with the functions prototypes. 0-linear

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* HEADERS */
+#include <stdio.h>
+
+/* DATA STRUCTURES*/
+/* PROTOTYPES OF FUNCTIONS */
+int linear_search(int *array, size_t size, int value);
+
+#endif
