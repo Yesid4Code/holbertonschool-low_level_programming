d302d7ada61297ac6ce8a2d301b81c1c45991ccd
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-26 18:30:26 +0000

    comment removed

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -22,8 +22,6 @@ int jump_search(int *array, size_t size, int value)
 		idx = jump;
 		jump = jump + sqrt(size);
 	}
-	/* if (idx >= size) */
-	/* return (-1);*/
 	printf("Value found between indexes [%lu] and [%lu]\n", idx, jump);
 	while (idx < size)
 	{
