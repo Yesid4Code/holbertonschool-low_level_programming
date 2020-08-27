4faee28f844f6c618783df3332af33aa5dc498cf
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-27 07:03:23 +0000

    Auxilar function added

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,40 @@
 #include "search_algos.h"
 
 /**
+ * auxbin - auxilar function.
+ * @array: Pointer to the firs element of the array.
+ * @low:   Position of the first element of the sub-array.
+ * @high:  Position of the last element of the sub-array.
+ * @value: Value to find in the array.
+ *
+ * Return: Index where value is located.
+ */
+int auxbin(int *array, size_t low, size_t high, int value)
+{
+	size_t i = 0, m = 0;
+
+	if (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i + 1 <= high)
+				printf(", ");
+		}
+		printf("\n");
+		m = low + (high - low) / 2;
+		if (array[m] == value && array[m - 1] != value)
+			return (m);
+		if (value <= array[m])
+			return (auxbin(array, low, m, value));
+		else if (value > array[m])
+			return (auxbin(array, (m + 1), high, value));
+	}
+	return (-1);
+}
+
+/**
  * advanced_binary - searches for a value
  * Binary search algorithm with recursion.
  * @array: Pointer to the firs element of the array.
@@ -11,25 +45,7 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, i = 0, m = 0;
-
-	if (array && size > 0)
-	{
-		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
-		{
-			printf("%d", array[i]);
-			if (i + 1 <= high)
-				printf(", ");
-		}
-		printf("\n");
-		m = (size - 1) / 2;
-		if (array[m] == value && array[m - 1] != value)
-			return (m);
-		if (value <= array[m])
-			return (advanced_binary(array, m + 1, value));
-		else if (value > array[m])
-			return (advanced_binary(array + m + 1, (size - m - 1), value));
-	}
-	return (-1);
+	if (!array)
+		return (-1);
+	return (auxbin(array, 0, (size - 1), value));
 }
