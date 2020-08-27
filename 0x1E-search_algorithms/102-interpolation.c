96dbe8c8826ca34ee81d8bb3de14551763943109
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-27 01:07:14 +0000

    '(' added on the math operation, on line 22

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,9 +17,9 @@ int interpolation_search(int *array, size_t size, int value)
 		while (low <= high)
 		{
 			pos = low +
-				(((double)(high - low) / array[high] - array[low]) *
+				(((double)(high - low) / (array[high] - array[low])) *
 				 (value - array[low]));
-			if (pos < 0 || pos >= size)
+			if (pos < low || pos > high)
 			{
 				printf("Value checked array[%lu] is out of range\n", pos);
 				break;
