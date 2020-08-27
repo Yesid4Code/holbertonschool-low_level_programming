e7eebb18c37405fb52375e4f65c5267b094aac4f
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-27 23:14:50 +0000

    jump search algorithm using on a simple linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * jump_search - search for a value - Jump search algorithm
- * @array: Pointer to the first element of the array.
+ * jump_list - search for a value - Jump search algorithm
+ * @list:  Pointer to the first element of the array.
  * @size:  Number of elements in the array.
  * @value: Value to search for.
  *
@@ -11,32 +11,31 @@
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t idx = 0, jump = sqrt(size);
-	int *head = *list, *temp = NULL;
+	listint_t *head = list, *temp = NULL;
 
 	if (!list)
 		return (NULL);
 	while (head->next)
 	{
-		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
 		temp = head;
-		while (temp->next && temp->index <= jump)
+		while (temp->next && temp->index < jump)
 			temp = temp->next;
-		if (!temp || temp->value >= value)
+		printf("Value checked array[%lu] = [%d]\n", jump, temp->n);
+		if (!temp || temp->n >= value)
 			break;
-		head = temp->next;
-
-		}
 		idx = jump;
 		jump = jump + sqrt(size);
 
+		head = temp;
+
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", idx, jump);
-	while (idx < size)
+	while (head->next)
 	{
-		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-		if (array[idx] == value)
-			return (idx);
-		idx++;
+		printf("Value checked array[%lu] = [%d]\n", idx, head->n);
+		if (head->n == value)
+			return (head);
+		head = head->next;
 	}
-	return (-1);
+	return (NULL);
 }
